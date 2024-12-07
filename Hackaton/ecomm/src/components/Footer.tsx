6be81f9449e073e502c1396d2a97eba8805e61c5
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-12-08 01:56:41 +0500

    init

---
+++ Hackaton/ecomm/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+const Footer = () => {
+  return (
+    <div className="flex">
+      <div className="flex h-[440px] w-[1512px] bg-[#000000]">
+<div className="h-[236px] w-[1170px] flex justify-center items-start gap-[87px] text-white">
+Exclusive
+</div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Footer;
