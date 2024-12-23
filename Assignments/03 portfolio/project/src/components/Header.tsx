ae356d30e6bde40898e14bd67361a896070e2cee
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-12-24 04:33:07 +0500

    header

--- Assignments/03 portfolio/project/src/components/Header.tsx
+++ Assignments/03 portfolio/project/src/components/Header.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
+import { Button } from './ui/button'
 
 export const Header = () => {
     return (
-        <div className='flex justify-between py-3 items-center bg-blue-900 text-white font-3xl '>
-            <div className='flex ml-8 justify-start '>Logo</div>
+        <div className='flex sm:flex-row sm:justify-between items-center bg-black text-white text-xl py-3 px-4'>
+             
+            <div className='mb-2 sm:mb-0 text-2xl'>Ayaz Ahmed</div>
 
-            <div className='flex mr-4 gap-11 items-center'>
+
+            <div className='flex gap-4 sm:gap-11 items-center'>
                 <Link href="/"> Home   </Link>
                 <Link href="/About"> About   </Link>
                 <Link href="/Services"> Services   </Link>
-                <Link href="Contact"> Contact Us  </Link>
+                <Link href="/Blog"> Blog  </Link>
+            </div>
+            
+            <div className='mt-2 sm:mt-0'>
+               <Button variant={'destructive'}> Contact Me </Button>
             </div>
 
 
