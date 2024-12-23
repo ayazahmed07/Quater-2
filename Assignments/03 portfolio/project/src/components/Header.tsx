e4d65d1482e15befac393fa90f52c6674b53287c
Ayaz ahmed <ayazahmed824@gmail.com>
2024-12-23 09:55:33 +0000

    updated Header

--- Assignments/03 portfolio/project/src/components/Header.tsx
+++ Assignments/03 portfolio/project/src/components/Header.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link'
 
 export const Header = () => {
     return (
-        <div className='flex'>
-            <div></div>
+        <div className='flex justify-between py-3 items-center bg-blue-900 text-white font-3xl '>
+            <div className='flex ml-8 justify-start '>Logo</div>
 
-            <div>
+            <div className='flex mr-4 gap-11 items-center'>
                 <Link href="/"> Home   </Link>
                 <Link href="/About"> About   </Link>
                 <Link href="/Services"> Services   </Link>
-                <Link href="Contact"> Contact   </Link>
+                <Link href="Contact"> Contact Us  </Link>
             </div>
 
 
