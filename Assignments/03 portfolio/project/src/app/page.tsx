4a5d2adbc70bfbb9232aa3c856f26637c4ee0e34
Ayaz ahmed <ayazahmed824@gmail.com>
2024-12-24 08:18:51 +0000

    homepage

--- Assignments/03 portfolio/project/src/app/page.tsx
+++ Assignments/03 portfolio/project/src/app/page.tsx
@@ -3,8 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
-  
+    <div className="flex h-screen bg-gray-900">
+      <div>
+        <p>Building Scalable Modern Websites for the Future</p>
+
+
+      </div>
+
     </div>
   );
 }
--- Assignments/03 portfolio/project/src/components/Header.tsx
+++ Assignments/03 portfolio/project/src/components/Header.tsx
@@ -4,17 +4,17 @@ import { Button } from './ui/button'
 
 export const Header = () => {
     return (
-        <div className='flex sm:flex-row sm:justify-between items-center bg-black text-white text-xl py-3 px-4'>
+        <div className=' flex sm:flex-row sm:justify-between items-center bg-black text-white text-xl py-2 px-3'>
              
-            <div className='mb-2 sm:mb-0 text-2xl'>Ayaz Ahmed</div>
+            <div className='mb-2 sm:mb-0 font-bold'>Ayaz Ahmed</div>
 
 
             
-                <div className='text-gray-400  border border-[#fafafa80] rounded-3xl py-1 px-5 flex gap-4 sm:gap-8 items-center'>
-                <Link className='bg-white p-2 rounded' href="/"> Home   </Link>
-                <Link href="/About"> About   </Link>
-                <Link href="/Services"> Services   </Link>
-                <Link href="/Blog"> Blog  </Link>
+                <div className='text-gray-500 border border-[#fafafa80] rounded-3xl py-1 px-5 flex gap-4 sm:gap-8 items-center'>
+                <Link className='bg-w hover:bg-white hover:text-black transition-colors duration-300 px-2 py-1 rounded md  ' href="/"> Home   </Link>
+                <Link className='hover:bg-white hover:text-black transition-colors duration-300 px-2 py-1 rounded md' href="/About"> About   </Link>
+                <Link className='hover:bg-white hover:text-black transition-colors duration-300 px-2 py-1 rounded md' href="/Services"> Services   </Link>
+                <Link className='hover:bg-white hover:text-black transition-colors duration-300 px-2 py-1 rounded md' href="/Blog"> Blog  </Link>
                 </div>
             
 
