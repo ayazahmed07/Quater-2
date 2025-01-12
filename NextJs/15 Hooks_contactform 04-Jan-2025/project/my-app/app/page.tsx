d0a2699819fa42674f477e44573a9c3830ef65cd
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2025-01-13 00:10:54 +0500

    cha
    change

---
+++ NextJs/15 Hooks_contactform 04-Jan-2025/project/my-app/app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Image from "next/image";
+import Header from "@/components/Header";
+
+
+export default function Home() {
+
+    return (
+    <div className="flex justify-center items-center h-screen">
+  Form
+
+         </div>
+  );
+}
