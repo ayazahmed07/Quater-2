76047c0d34bf397146cca127f56e165bab8bf7d7
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2025-01-26 19:27:10 +0500

    upto price

--- Hackatonmarketplace/marketplace/my-app/src/app/page.tsx
+++ Hackatonmarketplace/marketplace/my-app/src/app/page.tsx
@@ -1,34 +1,44 @@
-import { client } from "@/sanity/lib/client"
+import { client } from "@/sanity/lib/client";
 
-
-const getProducts = async ()=>{
+const getProducts = async () => {
   const products = await client.fetch(`
     *[_type == "product"][0..20] {
       _id,
     name,
-    description,
     price,
+    discountPercentage,
+    stockLevel,
+    isFeaturedProduct,
+    description,
+    
+    
     "image_url": image.asset->url,
 
     }
   `);
 
+  return products;
+};
 
-  return products
-}
-
-export default async function Home (){
-  const products = await getProducts()
+export default async function Home() {
+  const products = await getProducts();
   return (
-    <div>
+    <div className="flex flex-col gap-[100px]">
       {products.map((product: any) => (
         <div key={product._id}>
           <h2>Product Name: {product.name}</h2>
-          <p>Product Price: {product.price}</p>
-          <h2>Discount Percentage: {product.discountPercentage}</h2>
           <p>Product Description: {product.description}</p>
-          
-          <img src={product.image_url} alt={product.name} height={400} width={400} />
+          <div className="flex gap-10">
+            <img
+              src={product.image_url}
+              alt={product.name}
+              height={200}
+              width={200}
+            />
+          </div>
+          <p>Product Price: {product.price}</p>
+          <p>Discount Percentage: {product.discountPercentage}</p>
+          <p>StockLevel: {product.stockLevel}</p>
         </div>
       ))}
     </div>
