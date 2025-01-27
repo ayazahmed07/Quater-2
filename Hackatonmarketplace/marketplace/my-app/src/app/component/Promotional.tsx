4f8fe0b5f2f5325070a3ebef1784771eb0f78b63
Ayaz ahmed <ayazahmed824@gmail.com>
2025-01-27 09:41:37 +0000

    updated with promotional

---
+++ Hackatonmarketplace/marketplace/my-app/src/app/component/Promotional.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Promotional = () => {
+  return (
+    <div>
+      <div className="flex w-full h-[464px] bg-[#F2F0FF]"> 
+       
+    
+      </div>
+    </div>
+  )
+}
+
+export default Promotional
--- Hackatonmarketplace/marketplace/my-app/src/app/page.tsx
+++ Hackatonmarketplace/marketplace/my-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import getProducts from './component/Datafetch'
 import Productgrid from './component/Productgrid';
+import Promotional from './component/Promotional'; 
 
 
 export default async function Home() {
   const products = await getProducts();
   return (
     <div> 
+      <Promotional/>
 <Productgrid/>
     </div>
   );
