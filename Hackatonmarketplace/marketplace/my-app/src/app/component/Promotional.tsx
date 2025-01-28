dc02cd5a6c2231ac5c05da92e8054dede8c505e9
Ayaz ahmed <ayazahmed824@gmail.com>
2025-01-28 11:27:23 +0000

    with promo

--- Hackatonmarketplace/marketplace/my-app/src/app/component/Promotional.tsx
+++ Hackatonmarketplace/marketplace/my-app/src/app/component/Promotional.tsx
@@ -5,8 +5,22 @@ const Promotional = () => {
   return (
     <div>
       <div className="flex w-full h-[464px] bg-[#F2F0FF]"> 
-       
-    
+      <div className=''> 
+      <Image
+          src="/Images/lamp.jpg"
+          height={180}
+          width={180}
+          alt="Promotional"
+        />
+</div>
+<div className='flex justify-center item-center'>
+         <Image
+          src="/Images/sofapromotional.jpg"
+          height={450}
+          width={450}
+          alt="Sofa"
+        />  
+    </div>
       </div>
     </div>
   )
