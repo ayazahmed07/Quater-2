202c96ffbb010e44c0b499b7e75165526a6a2676
Ayaz ahmed <ayazahmed824@gmail.com>
2025-01-28 11:45:57 +0000

    banner

--- Hackatonmarketplace/marketplace/my-app/src/app/component/Promotional.tsx
+++ Hackatonmarketplace/marketplace/my-app/src/app/component/Promotional.tsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 const Promotional = () => {
   return (
     <div>
-      <div className="flex w-full h-[464px] bg-[#F2F0FF]"> 
-      <div className=''> 
-      <Image
-          src="/Images/lamp.jpg"
-          height={180}
-          width={180}
-          alt="Promotional"
-        />
-</div>
-<div className='flex justify-center item-center'>
-         <Image
-          src="/Images/sofapromotional.jpg"
-          height={450}
-          width={450}
-          alt="Sofa"
-        />  
-    </div>
+      {/* Outer Container */}
+      <div className="flex flex-col lg:flex-row w-full h-[auto] bg-[#F2F0FF] items-start justify-start p-0 gap-6 lg:gap-[550px]">
+        {/* Lamp Image */}
+        <div className="flex justify-center">
+          <Image
+            src="/Images/lamp.jpg"
+            height={220}
+            width={220}
+            alt="Promotional"
+            className="rounded-lg"
+          />
+        </div>
+
+                {/* Sofa Image */}
+        <div className="flex justify-center lg:ml-[50px] lg:h-[420px] mt-[20px] mb-10">
+          <Image
+            src="/Images/sofapromotional.jpg"
+            height={400}
+            width={450}
+            alt="Sofa"
+            className="rounded-lg"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Promotional
+export default Promotional;
