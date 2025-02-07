f2ed8e135bd378e5bf9fdfaca068b7b14fdf590a
Ayaz ahmed <ayazahmed824@gmail.com>
2025-02-07 11:37:42 +0000

    updated

--- Hackatonmarketplace/marketplace/my-app/src/app/component/Latestproduct.tsx
+++ Hackatonmarketplace/marketplace/my-app/src/app/component/Latestproduct.tsx
@@ -4,19 +4,52 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 const Latestproduct = () => {
+  const [activeLink, setActiveLink] = useState(""); // Track the currently active link
+
+  // Function to handle link click
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link); // Update the active link
+  };
+
   return (
     <div className="px-5 md:px-10">
       <div className="mt-[75px] flex text-2xl md:text-3xl text-[#1A0B5B] justify-center font-bold text-center">
         Latest Products
       </div>
       <div className="flex flex-wrap mt-3 mb-7 text-[#1A0B5B] items-center justify-center gap-4 md:gap-8 text-sm md:text-base">
-        <ul className="cursor-pointer">New Arrival</ul>
-        <ul className="cursor-pinter">Best Seller</ul>
-        <ul className="cursor-pointer">
+        <ul
+          className={`cursor-pointer ${
+            activeLink === "New Arrival" ? "text-red-800 underline" : "text-black "
+          } hover:text-red-400`}
+          onClick={() => handleLinkClick("New Arrival")}
+        >
+          New Arrival
+        </ul>
+        <ul
+          className={`cursor-pointer ${
+            activeLink === "Best Seller" ? "text-red-800 underline" : "text-black"
+          } hover:text-red-400`}
+          onClick={() => handleLinkClick("Best Seller")}
+        >
+          Best Seller
+        </ul>
+        <ul
+          className={`cursor-pointer ${
+            activeLink === "Featured" ? "text-red-800 underline" : "text-black"
+          } hover:text-red-400`}
+          onClick={() => handleLinkClick("Featured")}
+        >
           {" "}
           <Link href="/#featured-products">Featured </Link>{" "}
         </ul>
-        <ul className="cursor-pointer">Special Offer</ul>
+        <ul
+          className={`cursor-pointer ${
+            activeLink === "Special Offer" ? "text-red-800 underline" : "text-black"
+          } hover:text-red-400`}
+          onClick={() => handleLinkClick("Special Offer")}
+        >
+          Special Offer
+        </ul>
       </div>
       <div></div>
     </div>
