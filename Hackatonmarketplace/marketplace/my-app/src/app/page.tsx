a09e6ad943bce631229946a823625467654384f3
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2025-02-16 23:43:32 +0500

    yp

--- Hackatonmarketplace/marketplace/my-app/src/app/page.tsx
+++ Hackatonmarketplace/marketplace/my-app/src/app/page.tsx
@@ -1,35 +1,36 @@
-import getProducts from '@/components/Datafetch';
-import Featuredproducts from '@/components/Featuredproducts';
-import Latest from '@/components/Latestproducts';
-import Lpsection from '@/components/Lpsection';
-import Promotional from '@/components/Promotional'; 
-import Shopexsection from '@/components/Shopexsection';
-import Trendingproducts from '@/components/Trendingproducts';
-import Uniquefeatureproduct from '@/components/Uniquefeatureprod';
-import uniquefeatureproduct from '@/components/Uniquefeatureprod';
-
-
+import getProducts from "@/components/Datafetch";
+import Featuredproducts from "@/components/Featuredproducts";
+import Latest from "@/components/Latestproducts";
+import Lpsection from "@/components/Lpsection";
+import Promotional from "@/components/Promotional";
+import Shopexsection from "@/components/Shopexsection";
+import Trendingproducts from "@/components/Trendingproducts";
+import Uniquefeatureproduct from "@/components/Uniquefeatureprod";
 
 export default async function Home() {
   const products = await getProducts();
   return (
-    <div> 
-      
-      <Promotional/>
-      <div id="featured-products" className="mt-[75px] flex text-3xl text-[#1A0B5B] justify-center font-bold">
+    <div>
+      <Promotional />
+      <div
+        id="featured-products"
+        className="mt-[75px] flex text-3xl text-[#1A0B5B] justify-center font-bold"
+      >
         Featured Products
       </div>
-      
-<Featuredproducts/>
-<Lpsection/>
-<Latest/>
-<Shopexsection/>
-<Uniquefeatureproduct/>
-<div id="featured-products" className="mt-[75px] flex text-3xl text-[#1A0B5B] justify-center font-bold">
-        Tending Products
-      </div>
-<Trendingproducts/>
 
+      <Featuredproducts />
+      <Lpsection />
+      <Latest />
+      <Shopexsection />
+      <Uniquefeatureproduct />
+      <div
+        id="featured-products"
+        className="mt-[75px] flex text-3xl text-[#1A0B5B] justify-center font-bold"
+      >
+        Trending Products
+      </div>
+      <Trendingproducts />
     </div>
   );
 }
